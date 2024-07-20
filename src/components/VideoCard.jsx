97a4e57bcccacdcc5e4e49d2f960ec21563be679
Vivek Vinayak Abhale <vivek.abhale48@gmail.com>
2024-07-20 20:29:39 +0530

    Worked on video card and videoDetails page

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -4,50 +4,56 @@ import { Link } from "react-router-dom";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 import VideoLength from '../shared/VideoLength';
 
-const VideoCard = ({video}) => {
-
+const VideoCard = ({video, type}) => {
+  console.log(type)
   return (
     <Link to={`video/${video?.videoId}`}>
-      <div className='h-48 md:h-40 md:rounded-xl overflow-hidden relative'>
-        <img 
-        className='w-full h-full object-cover'
-        src={video?.thumbnails?.[0]?.url}/>
-        {
-          video?.lengthSeconds && (
-            <VideoLength time={video?.lengthSeconds}/>
-          )
-        }
-      </div>
-      <div className='flex mt-3'>
-        <div className='flex items-start'>
-          <div className='flex w-9 h-9 rounded-full overflow-hidden'>
-            <img className='w-full h-full object-cover' src={video?.author?.avatar[0]?.url} />
-          </div>
+      <div className={`${type === 'videoDetailsPage' ? 'flex mb-3' : ''}`}>
+        <div className={`${type === 'videoDetailsPage' ? 'min-w-[168px] h-[94px] md:rounded-xl overflow-hidden relative' : 'h-48 md:h-40 md:rounded-xl overflow-hidden relative'}`}>
+          <img 
+          className='w-full h-full object-cover'
+          src={video?.thumbnails?.[0]?.url}/>
+          {
+            video?.lengthSeconds && (
+              <VideoLength time={video?.lengthSeconds}/>
+            )
+          }
         </div>
-        <div className='flex flex-col ml-3 overflow-hidden'>
-          <span className='text-sm font-bold line-camp-2'>
-            {video?.title}
-          </span>
-          <div className='text-[12px] font-semibold text-white/[0.7] flex items-center mt-2'>
-            {video?.author?.title}
-            {video?.author?.badges[0]?.type === 'VERIFIED_CHANNEL' && (
-              <BsFillCheckCircleFill className='text-white/[0.5] text-[12px] ml-1'/>
-            )}
-          </div>
-          <div className='text-[12px] font-semibold text-white/[0.7] flex items-center mt-1'>
-            {
-              video?.stats?.views && (
-                <>
-                  <span>{`${abbreviateNumber(video?.stats?.views)} views`}</span>
-                  <span className="flex text-[24px] leading-none font-bold text-white/[0.7] relative top-[-8px] mx-1">
-                    .
-                  </span>
-                </>
-              )
-            }
-            <span className="truncate">
-              {video?.publishedTimeText}
+        <div className={`${type === 'videoDetailsPage' ? 'flex' : 'flex mt-3'}`}>
+          {
+            type !== 'videoDetailsPage' && (
+              <div className='flex items-start'>
+                <div className='flex w-9 h-9 rounded-full overflow-hidden'>
+                  <img className='w-full h-full object-cover' src={video?.author?.avatar[0]?.url} />
+                </div>
+              </div>
+            )
+          }
+          <div className='flex flex-col ml-3 overflow-hidden'>
+            <span className={`${type === 'videoDetailsPage' ? 'text-sm font-bold line-clamp2-css' : 'text-sm font-bold line-camp-2'}`}>
+              {video?.title}
             </span>
+            <div className='text-[12px] font-semibold text-white/[0.7] flex items-center mt-2'>
+              {video?.author?.title}
+              {video?.author?.badges[0]?.type === 'VERIFIED_CHANNEL' && (
+                <BsFillCheckCircleFill className='text-white/[0.5] text-[12px] ml-1'/>
+              )}
+            </div>
+            <div className='text-[12px] font-semibold text-white/[0.7] flex items-center mt-1'>
+              {
+                video?.stats?.views && (
+                  <>
+                    <span>{`${abbreviateNumber(video?.stats?.views)} views`}</span>
+                    <span className="flex text-[24px] leading-none font-bold text-white/[0.7] relative top-[-8px] mx-1">
+                      .
+                    </span>
+                  </>
+                )
+              }
+              <span className="truncate">
+                {video?.publishedTimeText}
+              </span>
+            </div>
           </div>
         </div>
       </div>
--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -8,11 +8,12 @@ import { abbreviateNumber } from "js-abbreviation-number";
 import { fetchDataFromApi } from "../utils/api";
 import { Context } from "../context/contextApi";
 import LeftNav from "./LeftNav";
+import VideoCard from "./VideoCard";
 
 const VideoDetails = () => {
   const [video, setVideo] = useState();
   const [similarVideo, setSimilarVideo] = useState();
-  const { setLoading, setMobileMenu } = useContext(Context);
+  const { loading, setLoading, setMobileMenu } = useContext(Context);
   const params = useParams();
   const id = params.videoId;
   useEffect(()=>{
@@ -34,15 +35,36 @@ const VideoDetails = () => {
     console.log(fetchSimilarVideoData);
     setSimilarVideo(fetchSimilarVideoData);
     setLoading(false);
-
+    // console.log(similarVideo);
   }
 
   return (
     <div className='flex absolute top-[60px] bottom-0 left-0 right-0'>
-      <LeftNav/>
-      <div className="flex-1">
-        <div></div>
-        <div></div>
+      <div className="flex-1 ml-6 pt-6 pr-6">
+        <div className="h-[483px]">
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${id}`}
+            controls
+            width="100%"
+            height="100%"
+          />
+        </div>
+      </div>
+      <div className="pt-6 pr-6 max-w-[402px]">
+        <div className="flex flex-col">
+          {
+            !loading && similarVideo && similarVideo.contents.map((content) =>{
+              if(content?.type !== 'video') return false;
+              return (
+                <VideoCard
+                  key={content?.video?.videoId}
+                  video={content?.video}
+                  type={'videoDetailsPage'}
+                />
+              )
+            })
+          }
+        </div>
       </div>
     </div>
   )
